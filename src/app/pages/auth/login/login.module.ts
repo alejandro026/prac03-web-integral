a9f42be9ec463fe952801c36d012065d0c6668e1
Alejandro Guerrero <92689016+alejandro026@users.noreply.github.com>
2023-06-08 00:03:35 -0600

    fix log

--- src/app/pages/auth/login/login.module.ts
+++ src/app/pages/auth/login/login.module.ts
@@ -1,9 +1,9 @@
+import { MaterialModule } from './../../../material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { MaterialModule } from 'src/app/material.module';
 
 
 @NgModule({
