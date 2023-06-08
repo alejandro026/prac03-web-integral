7cd7c6ae9343ecb851a9ef7c81258936f5941142
Alejandro Guerrero <92689016+alejandro026@users.noreply.github.com>
2023-06-08 00:26:25 -0600

    fix 3

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,6 @@
+import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
